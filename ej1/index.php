579a0884be776a08c730862568e3f3f076df7ddf
arya065 <arya065@g.educaand.es>
2023-09-20 14:02:43 +0200

    recogida done

--- ej1/index.php
+++ ej1/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
 </head>
+
 <body>
     <h1>Rellena tu CV</h1>
     <form action="recogida.php" method="post" enctype="multipart/form-data">
@@ -22,10 +24,10 @@
         <input type="text" id="dni" name="dni">
 
         <p>Sexo</p>
-        <input type="radio" id="hom" name="sexo">
+        <input type="radio" id="hom" name="sexo" value="Hombre">
         <label for="hom"><span>Hombre</span></label>
         <br><br>
-        <input type="radio" id="muj" name="sexo">
+        <input type="radio" id="muj" name="sexo" value="Mujer">
         <label for="muj"><span>Mujer</span></label>
 
         <p><label for="foto"><span>Incluir mi foto:</span></label>
@@ -33,7 +35,6 @@
         </p>
 
         <p><label for="ciudad"><span>Nacido en:</span></label>
-            
             <select name="ciudad" id="ciudad">
                 <option value="1">Malaga</option>
                 <option value="2">no Malga</option>
@@ -50,7 +51,9 @@
         <p><input type="checkbox" id="sub" name="sub"> <label for="sub">Subscribirse al bolerin de Novedades</label></p>
 
         <p><input type="submit" value="Guardar cambios">
-        <input type="reset" value="Borrar los datos introducidos"></p>
+            <input type="reset" value="Borrar los datos introducidos">
+        </p>
     </form>
 </body>
+
 </html>
--- ej1/recogida.php
+++ ej1/recogida.php
@@ -1,16 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recogida de datos</title>
 </head>
+
 <body>
     <h1>Recogiendo datos</h1>
     <?php
+    // //создание и перебор массива
+    // $a[0] = 3;
+    // $a[1] = 6;
+    // $a[2] = -1;
+    // $a[3] = "hola";
+
+    // for ($i = 0; $i < count($a); $i++) {
+    //     echo "<p>Numero: $a[$i] </p>";
+    // }
+
+
+
+    echo "<p><strong>Nombre:  </strong>" . $_POST["nombre"] . "</p>";
+    echo "<p><strong>Apellido:  </strong>" . $_POST["apellido"] . "</p>";
+    echo "<p><strong>Password:  </strong>" . $_POST["password"] . "</p>";
+    echo "<p><strong>DNI:  </strong>" . $_POST["dni"] . "</p>";
+
+    // isset вроде используется для того чтобы узнать если есть выбор
+    if (isset($_POST["sexo"])) {
+        echo "<p><strong>Sexo:  </strong>" . $_POST["sexo"] . "</p>";
+    } else {
+        echo "<p>No seleccionado</p>";
+    }
+
+    echo "<p><strong>Nacido:  </strong>" . $_POST["ciudad"] . "</p>";
+    echo "<p><strong>Comentario:  </strong>" . $_POST["comment"] . "</p>";
+
+    if (isset($_POST["sub"])) {
+        echo "<p><strong>Subscribirse:  </strong>" . $_POST["sub"] . "</p>";
+    } else {
+        echo "<p>No subs</p>";
+    }
+
     // $nombre = index.php
-        // echo "<p>nombre form:" .$nombre. "</p>";
+    // echo "<p>nombre form:" .$nombre. "</p>";
 
     ?>
 </body>
+
 </html>
