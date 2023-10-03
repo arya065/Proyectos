9ff6e8db4cdd54d2751209373ad56dc11ba5fc5a
arya065 <arya065@g.educaand.es>
2023-10-03 09:15:02 +0200

    ej1 done

--- strings prac/ej1.php
+++ strings prac/ej1.php
@@ -49,13 +49,17 @@ if (isset($_POST["compare"])) {
                 <label for="first">Primera palabra: </label>
                 <input type="text" id="first" name="first" value="<?php if (isset($_POST["first"])) echo $_POST["first"] ?>">
                 <?php
+                $_POST["first"] = trim($_POST["first"]);
                 if (isset($_POST["compare"])) {
                     if ($error_first) {
                 ?>
                         <span>*Campo Vacio*</span>
                 <?php
-                    } else {
+                    } else if (strlen($_POST["first"]) > 3) {
                         $first = substr($_POST["first"], strlen($_POST["first"]) - 3);
+                        echo "<h1>$first</h1>";
+                    } else {
+                        $first = $_POST["first"];
                     }
                 }
                 ?>
@@ -64,13 +68,18 @@ if (isset($_POST["compare"])) {
                 <label for="second">Secunda palabra: </label>
                 <input type="text" id="second" name="second" value="<?php if (isset($_POST["second"])) echo $_POST["second"] ?>">
                 <?php
+                $_POST["second"] = trim($_POST["second"]);
                 if (isset($_POST["compare"])) {
                     if ($error_second) {
                 ?>
                         <span>*Campo Vacio*</span>
                 <?php
-                    } else {
+                    } else if (strlen($_POST["second"]) > 3) {
+
                         $second = substr($_POST["second"], strlen($_POST["second"]) - 3);
+                        echo "<h1>$second</h1>";
+                    } else {
+                        $second = $_POST["second"];
                     }
                 }
                 ?>
@@ -84,26 +93,32 @@ if (isset($_POST["compare"])) {
     ?>
         <div class="results">
             <h1>Ripos - Resultado</h1>
-            <p>two words comparation</p>
+            <?php
+            if (strlen($first) > strlen($second)) {
+                $first = substr($first, strlen($first) - strlen($second));
+            } else if (strlen($first) < strlen($second)) {
+                $second = substr($second, strlen($second) - strlen($first));
+            }
+            if (isset($_POST["compare"]) && $first == $second && strlen($first) == 3 && strlen($second) == 3) {
+                echo "<p>" . $_POST["first"] . " y "  . $_POST["second"] . " se riman</p>";
+            } else if (isset($_POST["compare"]) && $first == $second && strlen($first) == 2 && strlen($second) == 2) {
+                echo "<p>" . $_POST["first"] . " y "  . $_POST["second"] . " se riman un poco</p>";
+            } else if (isset($_POST["compare"]) && $first == $second && strlen($first) == 1 && strlen($second) == 1) {
+                echo "<p>" . $_POST["first"] . " y "  . $_POST["second"] . " se riman muy poco</p>";
+            } else if (isset($_POST["compare"]) && substr($first, 1) == substr($second, 1) && strlen($first) == 3 && strlen($second) == 3) {
+                echo "<p>" . $_POST["first"] . " y "  . $_POST["second"] . " se riman un poco</p>";
+            } else if (isset($_POST["compare"]) && substr($first, 2) == substr($second, 2) && strlen($first) == 3 && strlen($second) == 3) {
+                echo "<p>" . $_POST["first"] . " y "  . $_POST["second"] . " se riman muy poco</p>";
+            } else {
+                echo "<p>no se riman</p>";
+            }
+            ?>
         </div>
     <?php
-    }
-    if (isset($_POST["compare"]) && $first == $second) {
-    ?>
-        <div>3 in a row</div>
-    <?php
-    }
-    if (isset($_POST["compare"]) && substr($first,1) == substr($second,1)) {
-    ?>
-    <div>2 in a row</div>
-    <?php
-    }    
-    if (isset($_POST["compare"]) && substr($first,2) == substr($second,2)) {
-    ?>
-    <div>1 in a row</div>
-    <?php
+
     }
     ?>
+
 </body>
 
 </html>
