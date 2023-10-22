0b74f6b8c8909c4e22816d3e292a00a346386bde
Alexey <arya065@g.educaand.es>
2023-10-22 14:10:06 +0200

    prueba ex1

---
+++ Prueba examen1/ejercicio1.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_POST["count"]) && $_POST["texto"] != "") {
+    echo "simoblos:",strlen($_POST["texto"]);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="ejercicio1.php" method="post" enctype="multipart/form-data">
+        <label for="texto">Introduce texto:</label>
+        <input type="text" name="texto" id="texto" value="<?php if (isset($_POST["texto"])) echo $_POST["texto"] ?>">
+        <br>
+        <input type="submit" value="Contar" name="count">
+    </form>
+</body>
+
+</html>
---
+++ Prueba examen1/ejercicio2.php
@@ -0,0 +1,42 @@
+<?php
+if (isset($_POST["send"])) {
+    if (correctFile($_FILES["fichero"])) {
+        @$fd = fopen("Ficheros/filename", "w");
+        fwrite($fd, file_get_contents($_FILES["fichero"]["tmp_name"]));
+        echo "Fichero subido";
+        fclose($fd);
+    } else {
+        echo "Error de subir";
+    }
+}
+function correctFile($file)
+{
+    if ($file["size"] > 1048576) {
+        return false;
+    }
+    if ($file["type"] != "text/plain") {
+        return false;
+    }
+    return true;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="ejercicio2.php" method="post" enctype="multipart/form-data">
+        <label for="fichero">Selecciona fichero de tipo .txt menor de 1MB</label>
+        <input type="file" name="fichero" id="fichero">
+        <br>
+        <input type="submit" value="Enviar" name="send">
+    </form>
+</body>
+
+</html>
