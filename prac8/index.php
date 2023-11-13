3d2950462f279e075b2b3cb1954af146382a6a7c
arya065 <arya065@g.educaand.es>
2023-11-13 10:00:11 +0100

    ini

---
+++ prac8/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 8</title>
+</head>
+
+<body>
+    <h1>Practica 8</h1>
+    <h2>Listado de los usuarios</h2>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th><a href="#">Usuario+</a></th><!--при нажатии открывает форму с добавлением пользователя-->
+        </tr>
+        <tr>
+            <td>Index</td>
+            <td>Photo if exist, if not, then photo with no photo</td>
+            <td><a href="#">Name</a></td>
+            <td><a href="#">Borrar</a> - <a href="#">Editar</a></td>
+        </tr>
+    </table>
+</body>
+
+</html>
