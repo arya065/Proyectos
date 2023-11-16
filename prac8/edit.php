daae96003313de86eb22b5b1b5f3d6ab341c4bbe
arya065 <arya065@g.educaand.es>
2023-11-16 13:47:46 +0100

    query done

--- prac8/edit.php
+++ prac8/edit.php
@@ -1,4 +1,14 @@
 <?php
+if (isset($_POST["send"])) {
+    $id = $_POST["id"];
+    foreach ($_POST as $key => $value) {
+        if (($value != "") && ($key != "id" && $key != "send")) {
+            change($id, $value, $key);
+        }
+    }
+    require "index.php";
+    return;
+}
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     try {
@@ -21,8 +31,8 @@ function show_info($conn, $id)
     }
     $line = mysqli_fetch_assoc($result);
 
-    echo '<form action="index.php" method="post">';
-    echo ' <p><label for="id">ID:</label><input type="text" name="id" id="id" placeholder="' . $line["id_usuario"] . '"></p>';
+    echo '<form action="edit.php" method="post">';
+    echo ' <p><label for="id">ID:</label><input type="text" name="id" id="id" value="' . $line["id_usuario"] . '" readonly></p>';
     echo '<p><label for="usuario">Usuario:</label><input type="text" name="usuario" id="usuario" placeholder="' . $line["usuario"] . '"></p>';
     echo '<p><label for="clave">Clave:</label><input type="password" name="clave" id="clave" placeholder="' . $line["clave"] . '"></p>';
     echo '<p><label for="nombre">Nombre:</label><input type="text" name="nombre" id="nombre" placeholder="' . $line["nombre"] . '"></p>';
@@ -30,12 +40,30 @@ function show_info($conn, $id)
     echo '<p><label for="sexo">Sexo:</label>';
     echo '<select name="sexo" id="sexo">';
     echo '<option hidden>' . $line["sexo"] . '</option>';
-    echo '<option value="hombre">Hombre</option>';
-    echo '<option value="mujer">Mujer</option>';
+    echo '<option value="hombre">hombre</option>';
+    echo '<option value="mujer">mujer</option>';
     echo '</select></p>';
-    echo '<p><label for="img">Imagen:</label><img src="img/' . $line["foto"] . '" alt="imagen usuario"><input type="file" name="img" id="img"></p>';
+    echo '<p><label for="img">Imagen:</label><img src="' . $line["foto"] . '" alt="imagen usuario"><br><input type="file" name="img" id="img"></p>';
+    echo '<input type="submit" value="Guardar" name="send">';
     echo '<button><a href="index.php">Volver</a></button>';
     echo '</form>';
+}
+function change($id, $value, $key)
+{
+    try {
+        $conn = mysqli_connect("localhost", "jose", "josefa", "bd_cv");
+        mysqli_set_charset($conn, "utf-8");
+    } catch (Exception $e) {
+        die("<p>no he podido connectarme:" . $e->getMessage() . "</p>");
+    }
+
+    try {
+        $consulta = "UPDATE usuarios SET " . $key . " = '" . $value . "' WHERE id_usuario='" . $id . "'";
+        $result = mysqli_query($conn, $consulta);
+    } catch (Exception $e) {
+        mysqli_close($conn);
+        die("<p>no he podido crear consulta:" . $e->getMessage() . "</p></body></html>");
+    }
 }
 ?>
 <!DOCTYPE html>
