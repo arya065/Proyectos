84eae43d3bcc4f5d7309435aaa0951f2aafde877
Alexey <arya065@g.educaand.es>
2023-11-29 21:13:23 +0100

    chg edit form place, add change query,add messages

--- prac9/index.php
+++ prac9/index.php
@@ -1,8 +1,82 @@
 <?php
 require("functions.php");
+session_start();
+
+if (isset($_SESSION["message"])) {
+    echo $_SESSION["message"];
+    unset($_SESSION["message"]);
+}
 if (isset($_POST["del"])) {
     deleteFilm($_POST["del"]);
     echo "eliminado con exito";
+    session_destroy();
+    session_start();
+    $_SESSION["message"] = "delete film";
+    header("Location: index.php");
+    exit;
+}
+if (isset($_POST["back"])) {
+    echo $_SESSION["id"];
+    $_POST["edit"] = $_SESSION["id"];
+}
+if (isset($_POST["delPhotoFin"])) {
+    try {
+        $conn = mysqli_connect(BD_SERVER, USER, PASS, BD_NAME);
+    } catch (Exception $e) {
+        echo "no se puede conectar a BD";
+        mysqli_close($conn);
+    }
+    try {
+        $result = mysqli_query($conn, "update peliculas set caratula='no_imagen.jpg' where idPelicula=" . $_SESSION['id'] . "");
+    } catch (Exception $e) {
+        echo "no se puede proceder query a BD";
+        mysqli_close($conn);
+    }
+    session_destroy();
+    session_start();
+    $_SESSION["message"] = "delete picture";
+    header("Location: index.php");
+    exit;
+}
+if (isset($_POST["home"])) {
+    session_destroy();
+    header("Location: index.php");
+    exit;
+}
+if (isset($_POST["saveChanges"])) {
+    $changes = false;
+    // $_SESSION["titulo"] = $titulo;
+    // $_SESSION["director"] = $director;
+    // $_SESSION["tematica"] = $tema;
+    // $_SESSION["sinopsis"] = $description;
+    // $_SESSION["caratula"] = $photo;
+    if ($_POST["titulo"] != $_SESSION["titulo"]) {
+        $changes = true;
+        change($_SESSION["id"], "titulo", $_POST["titulo"]);
+    }
+    if ($_POST["director"] != $_SESSION["director"]) {
+        $changes = true;
+        change($_SESSION["id"], "director", $_POST["director"]);
+    }
+    if ($_POST["tema"] != $_SESSION["tematica"]) {
+        $changes = true;
+        change($_SESSION["id"], "tematica", $_POST["tema"]);
+    }
+    if ($_POST["description"] != $_SESSION["sinopsis"]) {
+        $changes = true;
+        change($_SESSION["id"], "sinopsis", $_POST["description"]);
+    }
+    if ($changes) {
+        session_destroy();
+        session_start();
+        $_SESSION["message"] = "cambios guardados";
+    } else {
+        session_destroy();
+        session_start();
+        $_SESSION["message"] = "no hay nada para cambiar";
+    }
+    header("Location: index.php");
+    exit;
 }
 function getAllFilms()
 {
@@ -46,6 +120,22 @@ function getInfo($id)
     }
     try {
         $result = mysqli_query($conn, "select * from peliculas where idPelicula=$id");
+    } catch (Exception $e) {
+        echo "no se puede proceder query a BD";
+        mysqli_close($conn);
+    }
+    return $result;
+}
+function change($id, $key, $value)
+{
+    try {
+        $conn = mysqli_connect(BD_SERVER, USER, PASS, BD_NAME);
+    } catch (Exception $e) {
+        echo "no se puede conectar a BD";
+        mysqli_close($conn);
+    }
+    try {
+        $result = mysqli_query($conn, "update peliculas set $key=$value where idPelicula=$id");
     } catch (Exception $e) {
         echo "no se puede proceder query a BD";
         mysqli_close($conn);
@@ -107,26 +197,19 @@ function getInfo($id)
             }
             echo "<td>";
             echo '<form action="index.php" method="post"><button type="submit" name="del" value="' . $line["idPelicula"] . '">Borrar</button> - ';
-            echo '<button type="submit" name="edit" value="' . $line["idPelicula"] . '"><a href="views/edit.php?id=' . $line["idPelicula"] . '">Editar</a></button></form>';
+            echo '<button type="submit" name="edit" value="' . $line["idPelicula"] . '">Editar</button></form>';
             echo "</td>";
             echo "</tr>";
         }
         ?>
     </table>
     <?php
-    if (isset($_POST["delPhoto"])) {
-        echo "se dispone a cambiar la carratula <img src='img/" . $_POST["delPhoto"] . "' alt='old picture'> por esta otra: <img src='img/no_imagen.jpg' alt='new picture'>";
-        echo "<form action='index.php' method='post'>";
-        echo '<br>';
-        echo '<input type="submit" value="Continuar" name ="send">';
-        echo '<button><a href="views/edit.php">Atras</a></button>';
-        echo "</form>";
-    }
+
     // чтобы заработало, нужно убрать a href из кнопки
-    if (isset($_POST["edit"])) {
+    if (isset($_POST["edit"]) || (isset($_SESSION["id"]) && !isset($_POST["delPhoto"]))) {
     ?>
         <h1>Editar una pelicula</h1>
-        <form action="../index.php" method="post" enctype="multipart/form-data">
+        <form action="index.php" method="post" enctype="multipart/form-data">
             <?php
             if (isset($_SESSION["id"])) {
                 $films = mysqli_fetch_assoc(getInfo($_SESSION["id"]));
@@ -137,27 +220,36 @@ function getInfo($id)
             $titulo = $films["titulo"];
             $director = $films["director"];
             $tema = $films["tematica"];
-            $description = $films["sinopsis"];
             $photo = $films["caratula"];
+            $description = $films["sinopsis"];
             //save variables
             $_SESSION["titulo"] = $titulo;
             $_SESSION["director"] = $director;
             $_SESSION["tematica"] = $tema;
-            $_SESSION["sinopsis"] = $description;
             $_SESSION["caratula"] = $photo;
+            $_SESSION["sinopsis"] = $description;
             ?>
             <p>Titulo de la pelicula<br><input type="text" name="titulo" id="titulo" value="<?php echo $titulo ?>"></p>
             <p>Director de la pelicula<br><input type="text" name="director" id="director" value="<?php echo $director ?>"></p>
             <p>Tematica de la pelicula<br><input type="text" name="tema" id="tema" value="<?php echo $tema ?>"></p>
-            <p>Caratula Actual <br><img src="../img/<?php echo $photo ?>" alt="image"><br><button type="submit" value="<?php echo $_SESSION["caratula"] ?>" name="delPhoto">Eliminar Caratula</button></p>
+            <p>Caratula Actual <br><img src="img/<?php echo $photo ?>" alt="image"><br><button type="submit" value="<?php echo $_SESSION["caratula"] ?>" name="delPhoto">Eliminar Caratula</button></p>
             <p>Sinopsis de la pelicula</p>
             <textarea name="description" id="description" cols="30" rows="10"><?php echo $description ?></textarea><br>
             <p>Cambiar caratula de la pelicula:<input type="file" name="photo" id="photo"></p>
-            <input type="submit" value="Editar pelicula" name="send">
-            <button><a href="../index.php">Atras</a></button>
+            <input type="submit" value="Editar pelicula" name="saveChanges">
+            <button type="submit" value="<?php echo $_SESSION["id"] ?>" name="home">Atras</button>
             <br>
         </form>
     <?php
+    }
+    if (isset($_POST["delPhoto"])) {
+        echo "se dispone a cambiar la carratula <img src='img/" . $_POST["delPhoto"] . "' alt='old picture'> por esta otra: <img src='img/no_imagen.jpg' alt='new picture'>";
+        echo "<form action='index.php' method='post'>";
+        echo '<br>';
+        echo '<button type="submit" value = "' . $_SESSION["id"] . '" name="delPhotoFin">Continuar</button>';
+        echo '<button type="submit" value = "' . $_SESSION["id"] . '" name="backPhoto">Atras</button>';
+        // echo '<input type="submit" value="Atras" name="back">';
+        echo "</form>";
     }
     ?>
 </body>
