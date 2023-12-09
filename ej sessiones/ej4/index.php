3d5665a09fafed3dfe5eccb4ba1d82da732f1b9e
Alexey <arya065@g.educaand.es>
2023-12-09 17:45:13 +0100

    ini

--- ej sessiones/ej4/index.php
+++ ej sessiones/ej4/index.php
@@ -15,8 +15,8 @@ if (!isset($_SESSION["axis"]) || $_SESSION["axis"] == "") {
 
 <body>
     <form action="index2.php" method="post">
-        <button type="submit" name="left">-</button>
-        <button type="submit" name="right">+</button><br>
+        <button type="submit" name="left">&#x261C;</button>
+        <button type="submit" name="right">&#x261E;</button><br>
         <svg version="1.1" xmlns=http://www.w3.org/2000/svg width="600px" height="20px" viewbox="-300 0 600 20">
             <line x1="-300" y1="10" x2="300" y2="10" stroke="black" stroke-width="5" />
             <circle cx="<?php echo $_SESSION["axis"] ?>" cy="10" r="8" fill="red" />
