1a46a202178b9f1dd1ab18c4508c4ab3947c248b
Alexey <arya065@g.educaand.es>
2023-12-19 17:02:50 +0100

    little fix

--- ej sessiones/ex3Prueba/function.php
+++ ej sessiones/ex3Prueba/function.php
@@ -1,7 +1,7 @@
 <?php
 define("BD_SERVER", "localhost");
-define("USER", "jose");
-define("PASS", "josefa");
+define("USER", "root");
+define("PASS", "qwer");
 define("BD_NAME", "video_club");
 
 function query()
@@ -25,10 +25,14 @@ function LetraNIF($dni)
 {
     $letter = substr($_POST["dni"], strlen($_POST["dni"]) - 1);
     $dni = substr($_POST["dni"], 0, strlen($_POST["dni"]) - 1); //get only numbers
-    $valor = (int) ($dni / 23);
-    $valor *= 23;
-    $valor = $dni - $valor;
-    $letras = "TRWAGMYFPDXBNJZSQVHLCKEO";
-    $letraNif = substr($letras, $valor, 1);
-    return $letraNif == $letter;
+    if (is_numeric($dni)) {
+        $valor = (int) ($dni / 23);
+        $valor *= 23;
+        $valor = $dni - $valor;
+        $letras = "TRWAGMYFPDXBNJZSQVHLCKEO";
+        $letraNif = substr($letras, $valor, 1);
+        return $letraNif == $letter;
+    } else {
+        return false;
+    }
 }
