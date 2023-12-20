122f52fd17274c494116e94ca13d79091b355099
arya065 <arya065@g.educaand.es>
2023-12-20 13:59:38 +0100

    ini check if exist func

--- primerCRUD/function.php
+++ primerCRUD/function.php
@@ -2,4 +2,42 @@
 define("BD_SERVER", "localhost");
 define("USER", "jose");
 define("PASS", "josefa");
-define("BD_NAME", "bd_foro");
+define("BD_NAME", "bd_foro2");
+
+function query()
+{
+    try {
+        $conn = mysqli_connect("localhost", USER, PASS, BD_NAME);
+        mysqli_set_charset($conn, "utf8");
+    } catch (Exception $e) {
+        die("<p>no he podido connectarme:" . $e->getMessage() . "</p>");
+    }
+    try {
+        $consulta = "query";
+        $result = mysqli_query($conn, $consulta);
+    } catch (Exception $e) {
+        mysqli_close($conn);
+        die("<p>no hacer query:" . $e->getMessage() . "</p></body></html>");
+    }
+    return $result;
+}
+function ifExist($user, $pass)
+{
+    try {
+        $conn = mysqli_connect("localhost", USER, PASS, BD_NAME);
+        mysqli_set_charset($conn, "utf8");
+    } catch (Exception $e) {
+        die("<p>no he podido connectarme:" . $e->getMessage() . "</p>");
+    }
+    try {
+        $consulta = "select * from usuarios where usuario='$user' and clave='$pass'";
+        $result = mysqli_query($conn, $consulta);
+    } catch (Exception $e) {
+        mysqli_close($conn);
+        die("<p>no hacer query:" . $e->getMessage() . "</p></body></html>");
+    }
+    if (mysqli_num_rows($result) > 0) {
+        return true;
+    }
+    return false;
+}
--- primerCRUD/index.php
+++ primerCRUD/index.php
@@ -1,11 +1,72 @@
+<?php
+require "function.php";
+session_start();
+if (isset($_POST["enter"])) {
+    $errUser = $errPass = false;
+    if ($_POST["usuario"] == "") {
+        $errUser = true;
+    }
+    if ($_POST["clave"] == "") {
+        $errPass = true;
+    }
+    $errForm =  $errUser || $errPass;
+
+    if (!$errForm && !ifExist($_POST["usuario"], $_POST["clave"])) {
+        $errAccess = true;
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        span {
+            color: red;
+        }
+
+        span::before {
+            content: "*";
+        }
+
+        span::after {
+            content: "*";
+        }
+    </style>
 </head>
+
 <body>
-    
+    <form action="index.php" method="post">
+        <p>
+            <label for="usuario">Usuario:</label>
+            <input type="text" name="usuario" id="usuario" value="<?php if (isset($_POST["enter"])) echo $_POST["usuario"] ?>">
+            <?php
+            if (isset($errUser) && $errUser) {
+
+                echo "<span>error usuario</span>";
+            }
+            ?>
+        </p>
+        <p>
+            <label for="clave">Clave:</label>
+            <input type="text" name="clave" id="clave" value="<?php if (isset($_POST["enter"])) echo $_POST["clave"] ?>">
+            <?php
+            if (isset($errUser) && $errUser) {
+                echo "<span>error clave</span>";
+            }
+            ?>
+        </p>
+        <?php
+        if (isset($errAccess) && $errAccess) {
+            echo "<span>error de acceso</span><br>";
+        }
+        ?>
+        <button type="submit" name="enter">Entrar</button>
+    </form>
 </body>
+
 </html>
