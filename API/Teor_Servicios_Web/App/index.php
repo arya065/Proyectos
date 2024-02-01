0a034ce2b358aa0e2dcbba2c6cdc6bb2d01f6ed9
arya065 <arya065@g.educaand.es>
2024-02-01 16:20:18 +0100

    comm options

--- API/Teor_Servicios_Web/App/index.php
+++ API/Teor_Servicios_Web/App/index.php
@@ -1,83 +1,96 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teoría Servicios Webs</title>
 </head>
+
 <body>
     <?php
-    define("DIR_SERV","http://localhost/Proyectos/API/Teor_Servicios_Web/primera_api");
+    // Берём текущий код и устанавливаем новый
+    var_dump(http_response_code(201));
+    // Берём новый код
+    var_dump(http_response_code());
+
+    define("DIR_SERV", "http://localhost/Proyectos/API/Teor_Servicios_Web/primera_api");
 
     function consumir_servicios_REST($url, $metodo, $datos = null)
-{
-    $llamada = curl_init();
-    curl_setopt($llamada, CURLOPT_URL, $url);
-    curl_setopt($llamada, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($llamada, CURLOPT_CUSTOMREQUEST, $metodo);
-    if (isset($datos)) {
-        curl_setopt($llamada, CURLOPT_POSTFIELDS, http_build_query($datos));
+    {
+        $llamada = curl_init();
+        curl_setopt($llamada, CURLOPT_URL, $url);
+        curl_setopt($llamada, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($llamada, CURLOPT_CUSTOMREQUEST, $metodo);
+        if (isset($datos)) {
+            curl_setopt($llamada, CURLOPT_POSTFIELDS, http_build_query($datos));
+        }
+
+        // Добавим следующую строку для вывода дополнительной информации
+        curl_setopt($llamada, CURLOPT_VERBOSE, true);
+
+        $respuesta = curl_exec($llamada);
+
+        // Добавим следующие строки для вывода ошибок, если они есть
+        if ($respuesta === false) {
+            echo 'cURL error: ' . curl_error($llamada);
+        }
+
+        curl_close($llamada);
+        return $respuesta;
     }
 
-    // Добавим следующую строку для вывода дополнительной информации
-    curl_setopt($llamada, CURLOPT_VERBOSE, true);
 
-    $respuesta = curl_exec($llamada);
-
-    // Добавим следующие строки для вывода ошибок, если они есть
-    if ($respuesta === false) {
-        echo 'cURL error: ' . curl_error($llamada);
+    $url = DIR_SERV . "/saludo";
+    //$respuesta=file_get_contents($url);
+    $respuesta = consumir_servicios_REST($url, "GET");
+    print_r($respuesta);
+    // echo $respuesta;
+    $obj = json_decode($respuesta);
+    if (!$obj) {
+        die("<p>Error consumiendo el servicio1: " . $url . "<p>" . $respuesta);
+    } else {
+        echo "<p>El mensaje recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
     }
-
-    curl_close($llamada);
-    return $respuesta;
-}
-
-
-    $url=DIR_SERV."/saludo";
+    // ///////////////////////////////////////////////////////////////////////////////////////////////
+    $url = DIR_SERV . "/saludo/" . urlencode("Maria Antonia");
     //$respuesta=file_get_contents($url);
-    $respuesta=consumir_servicios_REST($url,"GET");
-    $obj=json_decode($respuesta);
-    if(!$obj)
-        die("<p>Error consumiendo el servicio1: ".$url."<p>".$respuesta);
+    $respuesta = consumir_servicios_REST($url, "GET");
+    $obj = json_decode($respuesta);
+    if (!$obj)
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
 
-    echo "<p>El mensaje recibido ha sido <strong>".$obj->mensaje."</strong></p>";
+    echo "<p>El mensaje recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
 
-    $url=DIR_SERV."/saludo/".urlencode("María Antonia");
-    //$respuesta=file_get_contents($url);
-    $respuesta=consumir_servicios_REST($url,"GET");
-    $obj=json_decode($respuesta);
-    if(!$obj)
-        die("<p>Error consumiendo el servicio: ".$url."<p>".$respuesta);
+    $url = DIR_SERV . "/saludo";
+    $datos["nombre"] = "Juan Alonso";
+    $respuesta = consumir_servicios_REST($url, "POST", $datos);
+    $obj = json_decode($respuesta);
+    if (!$obj)
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
 
-    echo "<p>El mensaje recibido ha sido <strong>".$obj->mensaje."</strong></p>";
-
-    $url=DIR_SERV."/saludo";
-    $datos["nombre"]="Juan Alonso";
-    $respuesta=consumir_servicios_REST($url,"POST",$datos);
-    $obj=json_decode($respuesta);
-    if(!$obj)
-        die("<p>Error consumiendo el servicio: ".$url."<p>".$respuesta);
-
-    echo "<p>El mensaje recibido ha sido <strong>".$obj->mensaje."</strong></p>";
+    echo "<p>El mensaje recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
 
 
-    $url=DIR_SERV."/borrar_saludo/37";
-    $respuesta=consumir_servicios_REST($url,"DELETE");
-    $obj=json_decode($respuesta);
-    if(!$obj)
-        die("<p>Error consumiendo el servicio: ".$url."<p>".$respuesta);
+    $url = DIR_SERV . "/borrar_saludo/37";
+    $respuesta = consumir_servicios_REST($url, "DELETE");
+    $obj = json_decode($respuesta);
+    if (!$obj)
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
 
-    echo "<p>El mensaje recibido ha sido <strong>".$obj->mensaje."</strong></p>";
+    echo "<p>El mensaje recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
 
-    $url=DIR_SERV."/actualizar_saludo/78";
-    $datos["nombre"]="Pepe Pérez";
-    $respuesta=consumir_servicios_REST($url,"PUT",$datos);
-    $obj=json_decode($respuesta);
-    if(!$obj)
-        die("<p>Error consumiendo el servicio: ".$url."<p>".$respuesta);
+    $url = DIR_SERV . "/actualizar_saludo/78";
+    $datos["nombre"] = "Pepe Pérez";
+    $respuesta = consumir_servicios_REST($url, "PUT", $datos);
+    $obj = json_decode($respuesta);
+    if (!$obj)
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
 
-    echo "<p>El mensaje recibido ha sido <strong>".$obj->mensaje."</strong></p>";
+    echo "<p>El mensaje recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
+
+
     ?>
 </body>
+
 </html>
--- API/Teor_Servicios_Web/primera_api/index.php
+++ API/Teor_Servicios_Web/primera_api/index.php
@@ -1,46 +1,50 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require __DIR__ . '/Slim/autoload.php';
+// $configuration = [
+//     'settings' => [
+//         'displayErrorDetails' => true,
+//         'outputBuffering' => false,
+//         'addContentLengthHeader' => false
+//     ],
+// ];
 
-$app= new \Slim\App;
+// $app = new \Slim\App($configuration);
+$app = new \Slim\App;
 
 
-$app->get('/saludo',function(){
-
-    $respuesta["mensaje"]="Hola";
+$app->get('/saludo', function () {
+    $respuesta["mensaje"] = "Hola";
     echo json_encode($respuesta);
 });
 
-$app->get('/saludo/{nombre}',function($request){
-
-    $valor_recibido=$request->getAttribute('nombre');
-    $respuesta["mensaje"]="Hola ".$valor_recibido;
+$app->get('/saludo/{nombre}', function ($request) {
+    $valor_recibido = $request->getAttribute('nombre');
+    $respuesta["mensaje"] = "Hola " . $valor_recibido;
     echo json_encode($respuesta);
 });
 
-$app->post('/saludo',function($request){
-
-    $valor_recibido=$request->getParam('nombre');
-    $respuesta["mensaje"]="Hola ".$valor_recibido;
+$app->post('/saludo', function ($request) {
+    $valor_recibido = $request->getParam('nombre');
+    $respuesta["mensaje"] = "Hola " . $valor_recibido;
     echo json_encode($respuesta);
 });
 
-$app->delete('/borrar_saludo/{id}',function($request){
-
-    $id_recibida=$request->getAttribute('id');
-    $respuesta["mensaje"]="Se ha borrado el saludo con id:".$id_recibida;
+$app->delete('/borrar_saludo/{id}', function ($request) {
+    $id_recibida = $request->getAttribute('id');
+    $respuesta["mensaje"] = "Se ha borrado el saludo con id:" . $id_recibida;
     echo json_encode($respuesta);
 });
 
-$app->put('/actualizar_saludo/{id}',function($request){
-
-    $id_recibida=$request->getAttribute('id');
-    $nombre_nuevo=$request->getParam('nombre');
-    $respuesta["mensaje"]="Se ha actualizado el saludo con id:".$id_recibida." al nombre: ".$nombre_nuevo;
+$app->put('/actualizar_saludo/{id}', function ($request) {
+    $id_recibida = $request->getAttribute('id');
+    $nombre_nuevo = $request->getParam('nombre');
+    $respuesta["mensaje"] = "Se ha actualizado el saludo con id:" . $id_recibida . " al nombre: " . $nombre_nuevo;
     echo json_encode($respuesta);
 });
 
 
 $app->run();
-
-?>
