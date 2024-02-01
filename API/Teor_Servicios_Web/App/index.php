e1eeef0b9b91ac4bc86f8daae8ed7eb774f9527b
arya065 <arya065@g.educaand.es>
2024-02-01 13:31:10 +0100

    ini

--- API/Teor_Servicios_Web/App/index.php
+++ API/Teor_Servicios_Web/App/index.php
@@ -9,18 +9,29 @@
     <?php
     define("DIR_SERV","http://localhost/Proyectos/API/Teor_Servicios_Web/primera_api");
 
-    function consumir_servicios_REST($url,$metodo,$datos=null)
-    {
-        $llamada=curl_init();
-        curl_setopt($llamada,CURLOPT_URL,$url);
-        curl_setopt($llamada,CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($llamada,CURLOPT_CUSTOMREQUEST,$metodo);
-        if(isset($datos))
-            curl_setopt($llamada,CURLOPT_POSTFIELDS,http_build_query($datos));
-        $respuesta=curl_exec($llamada);
-        curl_close($llamada);
-        return $respuesta;
+    function consumir_servicios_REST($url, $metodo, $datos = null)
+{
+    $llamada = curl_init();
+    curl_setopt($llamada, CURLOPT_URL, $url);
+    curl_setopt($llamada, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($llamada, CURLOPT_CUSTOMREQUEST, $metodo);
+    if (isset($datos)) {
+        curl_setopt($llamada, CURLOPT_POSTFIELDS, http_build_query($datos));
     }
+
+    // Добавим следующую строку для вывода дополнительной информации
+    curl_setopt($llamada, CURLOPT_VERBOSE, true);
+
+    $respuesta = curl_exec($llamada);
+
+    // Добавим следующие строки для вывода ошибок, если они есть
+    if ($respuesta === false) {
+        echo 'cURL error: ' . curl_error($llamada);
+    }
+
+    curl_close($llamada);
+    return $respuesta;
+}
 
 
     $url=DIR_SERV."/saludo";
@@ -28,7 +39,7 @@
     $respuesta=consumir_servicios_REST($url,"GET");
     $obj=json_decode($respuesta);
     if(!$obj)
-        die("<p>Error consumiendo el servicio: ".$url."<p>".$respuesta);
+        die("<p>Error consumiendo el servicio1: ".$url."<p>".$respuesta);
 
     echo "<p>El mensaje recibido ha sido <strong>".$obj->mensaje."</strong></p>";
 
