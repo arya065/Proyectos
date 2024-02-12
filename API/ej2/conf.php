9b6e224f82a0368819bd7b1345bc907f5d62cb79
arya065 <arya065@g.educaand.es>
2024-02-12 09:24:44 +0100

    ini

---
+++ API/ej2/conf.php
@@ -0,0 +1,6 @@
+<?php
+// define("DIR_SERV", "http://proyectos/API/ej1/primera_api");
+define("DIR_SERV", "http://localhost/Proyectos/API/ej1/primera_api");
+define("USER", "jose");
+define("PASS", "josefa");
+define("DB_NAME", "bd_tienda");
