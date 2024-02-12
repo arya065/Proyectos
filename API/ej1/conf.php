acef030c6e899ebd287d516fb9a584f1d65e71a6
arya065 <arya065@g.educaand.es>
2024-02-12 08:31:11 +0100

    ch conf

--- API/ej1/conf.php
+++ API/ej1/conf.php
@@ -1,7 +1,6 @@
 <?php
-define("DIR_SERV", "http://proyectos/API/ej1/primera_api");
-// http://proyectos/API/ej1/primera_api
-// http://localhost/Proyectos/API/ej1/primera_api
-define("USER", "root");
-define("PASS", "");
+// define("DIR_SERV", "http://proyectos/API/ej1/primera_api");
+define("DIR_SERV", "http://localhost/Proyectos/API/ej1/primera_api");
+define("USER", "jose");
+define("PASS", "josefa");
 define("DB_NAME", "bd_tienda");
