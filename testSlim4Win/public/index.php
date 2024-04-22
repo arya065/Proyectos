8f97ea208eb78a04e54bcd92d860d313933e3ebf
arya065 <arya065@g.educaand.es>
2024-04-22 12:01:25 +0200

    doesnt work

--- testSlim4Win/public/index.php
+++ testSlim4Win/public/index.php
@@ -10,15 +10,30 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->setBasePath('/proyectos/testSlim4Win/public');
 
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello Slim!");
+    return $response;
+});
 $app->get('/test', function (Request $request, Response $response, $args) {
-    echo json_encode(array("hi"=>"there"));
+    echo json_encode(array ("hi" => "there"));
     // $response->getBody()->write("Hello world!");
     return $response;
 });
-$app->get('/test2', function ($request,$response) {
-    $response->getBody()->write(json_encode(array("hi"=>"there")));
+$app->get('/test2', function ($request, $response) {
+    $response->getBody()->write(json_encode(array ("hi" => "there")));
     // return $response;
     return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
+
+/*
+Uncaught Slim\Exception\HttpNotFoundException: Not found.
+in C:\OSPanel\domains\Proyectos\testSlim4\vendor\slim\slim\Slim\Middleware\RoutingMiddleware.php:76 
+Stack trace: #0 C:\OSPanel\domains\Proyectos\testSlim4\vendor\slim\slim\Slim\Routing\RouteRunner.php(56):
+Slim\Middleware\RoutingMiddleware->performRouting() #1 C:\OSPanel\domains\Proyectos\testSlim4\vendor\slim\slim\Slim\MiddlewareDispatcher.php(65):
+Slim\Routing\RouteRunner->handle() #2 C:\OSPanel\domains\Proyectos\testSlim4\vendor\slim\slim\Slim\App.php(199): 
+Slim\MiddlewareDispatcher->handle() #3 C:\OSPanel\domains\Proyectos\testSlim4\vendor\slim\slim\Slim\App.php(183): 
+Slim\App->handle() #4 C:\OSPanel\domains\Proyectos\testSlim4\public\index.php(25): 
+Slim\App->run() #5 {main} thrown in C:\OSPanel\domains\Proyectos\testSlim4\vendor\slim\slim\Slim\Middleware\RoutingMiddleware.php on line 76
+*/
