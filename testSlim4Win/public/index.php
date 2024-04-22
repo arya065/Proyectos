4598735d36e6c19dc88b8c0089578d1e1a44e3ad
arya065 <arya065@g.educaand.es>
2024-04-22 11:41:14 +0200

    fix path

--- testSlim4Win/public/index.php
+++ testSlim4Win/public/index.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 
 require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
-$app->setBasePath('/proyectos/testSlim4/public');
+$app->setBasePath('/proyectos/testSlim4Win/public');
 
 $app->get('/test', function (Request $request, Response $response, $args) {
     echo json_encode(array("hi"=>"there"));
