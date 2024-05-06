4d937a5d9bf090a4e2adacef10bd7a2340982e34
arya065 <arya065@g.educaand.es>
2024-05-06 09:01:40 +0200

    fix ini

--- testSlim4/public/index.php
+++ testSlim4/public/index.php
@@ -3,22 +3,26 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
-// $app->setBasePath('/Proyectos/testSlim4/public');
-$app->setBasePath('/proyectos/testSlim4/public');
 
-$app->get('/test', function (Request $request, Response $response, $args) {
-    echo json_encode(array("hi"=>"there"));
-    // $response->getBody()->write("Hello world!");
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(false, true, true);
+
+$app->setBasePath('/Proyectos/testSlim4/public');
+
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
     return $response;
 });
-$app->get('/test2', function ($request,$response) {
-    $response->getBody()->write(json_encode(array("hi"=>"there")));
-    // return $response;
+
+$app->get('/test', function (Request $request, Response $response, $args) {
+    echo json_encode(array ("hi" => "there"));
+    return $response;
+});
+$app->get('/test2', function ($request, $response) {
+    $response->getBody()->write(json_encode(array ("hi" => "there")));
     return $response->withHeader('Content-Type', 'application/json');
 });
 
