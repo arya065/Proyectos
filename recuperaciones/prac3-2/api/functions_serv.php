dc610526580731cf448c37ae42d729e57b7ca40f
arya065 <arya065@g.educaand.es>
2024-05-22 13:39:41 +0200

    some ini func_serv

--- recuperaciones/prac3-2/api/functions_serv.php
+++ recuperaciones/prac3-2/api/functions_serv.php
@@ -15,13 +15,13 @@ function createConn()
         echo "No ha podido crear conexion: " . $e->getMessage();
     }
 }
-function example($code)
+function example()
 {
     try {
         $conn = createConn();
-        $sql = "SELECT * from producto where cod=?";
+        $sql = "SELECT * from categorias";
         $stmt = $conn->prepare($sql);
-        $stmt->execute([$code]);
+        $stmt->execute();
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
         $conn = null;
         return json_encode(array("answer" => $res));
@@ -30,3 +30,100 @@ function example($code)
         return json_encode(array("error" => $e->getMessage()));
     }
 }
+function login($user, $pass)
+{
+    try {
+        $conn = createConn();
+        $sql = "SELECT * from usuarios where usuario=? and clave=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute([$user, $pass]);
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $conn = null;
+        if (count($res)) {
+            session_name("api_func");
+            session_start();
+            $session_api = session_id();
+            $_SESSION["usuario"] = $user;
+        }
+        return json_encode(array("response" => $res, "session_api" => $session_api));
+    } catch (PDOException $e) {
+        $conn = null;
+        return json_encode(array("error" => $e->getMessage()));
+    }
+}
+function insertarUsuario($user, $pass, $email, $api_session)
+{
+    try {
+        session_name("api_func");
+        session_id($api_session);
+        session_start();
+        if (isset($_SESSION["usuario"])) {
+            echo "exist<br>";
+            $conn = createConn();
+            $sql = "INSERT into usuarios (usuario,clave,email) values (?,?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$user, $pass, $email]);
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $conn = null;
+            return json_encode(array("response" => $res));
+        } else {
+            return json_encode(array("response" => "session id is not correct"));
+        }
+    } catch (PDOException $e) {
+        $conn = null;
+        return json_encode(array("error" => $e->getMessage()));
+    }
+}
+function usuarios($columna, $valor, $api_session)
+{
+    try {
+        session_name("api_func");
+        session_id($api_session);
+        session_start();
+        if (isset($_SESSION["usuario"])) {
+            $conn = createConn();
+            $sql = "SELECT * from usuarios where $columna =?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$valor]);
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $conn = null;
+            return json_encode(array("response" => $res));
+        } else {
+            return json_encode(array("response" => "session id is not correct"));
+        }
+    } catch (PDOException $e) {
+        $conn = null;
+        return json_encode(array("error" => $e->getMessage()));
+    }
+}
+function comentarios($id, $api_session)
+{
+    try {
+        session_name("api_func");
+        session_id($api_session);
+        session_start();
+        if (isset($_SESSION["usuario"])) {
+            $conn = createConn();
+            $sql = "SELECT * from comentarios where idComentario =?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$id]);
+            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $conn = null;
+            return json_encode(array("response" => $res));
+        } else {
+            return json_encode(array("response" => "session id is not correct"));
+        }
+    } catch (PDOException $e) {
+        $conn = null;
+        return json_encode(array("error" => $e->getMessage()));
+    }
+}
+
+/**
+ * $app->get("/usuario/{id}", function () { });
+ * $app->get("/noticia/{id}", function () { });
+ * $app->get("/categoria/{id}", function () { });
+ * $app->get("/comentario/{id}", function () { });
+ * $app->get("/actualizarComentario/{id}", function () { });
+ * $app->get("/borrarComentario/{id}", function () { });
+ */
--- recuperaciones/prac3-2/api/index.php
+++ recuperaciones/prac3-2/api/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/Slim/autoload.php';
 $app = new \Slim\App;
 
 $app->get("/example", function () {
-    echo getProductos();
+    echo example();
 });
 $app->get("/example/{codigo}", function ($request) {
     echo getProductosCode($request->getAttribute("codigo"));
@@ -13,14 +13,27 @@ $app->put("/example/insertar", function ($request) {
     echo insertProducto($request->getParam("codigo"), $request->getParam("nombre"), $request->getParam("nombreCorto"), $request->getParam("descripcion"), $request->getParam("pvp"), $request->getParam("familia"));
 });
 
-$app->get("/login", function () { });
-$app->get("/insertarUsuario", function () { });
-$app->get("/usuarios/{columna}/{valor}", function () { });
-$app->get("/comentarios/{id}", function () { });
-$app->get("/usuario/{id}", function () { });
-$app->get("/noticia/{id}", function () { });
-$app->get("/categoria/{id}", function () { });
-$app->get("/comentario/{id}", function () { });
-$app->get("/actualizarComentario/{id}", function () { });
-$app->get("/borrarComentario/{id}", function () { });
+$app->get("/login", function ($request) {
+    // echo login($request->getParam("usuario"), $request->getParam("clave"));
+    echo login("1", "1");
+});
+$app->get("/insertarUsuario", function ($request) {
+    // $api_session = $request->getParam("api_session");
+    // echo insertarUsuario($request->getParam("usuario"), $request->getParam("clave"), $request->getParam("email"), $api_session);
+    echo insertarUsuario("3", "3", "3", "t53iqrkf07fh7jq6qjgcv8o213");
+});
+$app->get("/usuarios/{columna}/{valor}", function ($request) {
+    // $api_session = $request->getParam("api_session");
+    echo usuarios($request->getAttribute("columna"), $request->getAttribute("valor"), "t53iqrkf07fh7jq6qjgcv8o213");
+});
+$app->get("/comentarios/{id}", function ($request) {
+    // $api_session = $request->getParam("api_session");
+    echo comentarios($request->getAttribute("id"), "t53iqrkf07fh7jq6qjgcv8o213");
+});
+$app->get("/usuario/{id}", function ($request) { });
+$app->get("/noticia/{id}", function ($request) { });
+$app->get("/categoria/{id}", function ($request) { });
+$app->get("/comentario/{id}", function ($request) { });
+$app->get("/actualizarComentario/{id}", function ($request) { });
+$app->get("/borrarComentario/{id}", function ($request) { });
 $app->run();
