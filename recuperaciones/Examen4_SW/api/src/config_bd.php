2e4403c1d129f1e96a6a22a3d5a8d2f7531aa8ba
arya065 <arya065@g.educaand.es>
2024-06-07 15:18:45 +0200

    cng cfg

--- recuperaciones/Examen4_SW/api/src/config_bd.php
+++ recuperaciones/Examen4_SW/api/src/config_bd.php
@@ -1,7 +1,7 @@
 <?php
 define("SERVIDOR_BD", "localhost");
-define("USUARIO_BD", "jose");
-define("CLAVE_BD", "josefa");
+define("USUARIO_BD", "root");
+define("CLAVE_BD", "");
 define("NOMBRE_BD", "bd_exam_colegio2");
 define("INACTIVE_TIME", 15 * 60);
 define("LAST_ACTIVE", time());
